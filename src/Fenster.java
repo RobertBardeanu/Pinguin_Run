28f87fea58915129f859390c813956cc5333fa6e
RobertBardeanu <robert.bardeanu@gmx.de>
2026-01-13 08:04:40 +0100

    git push --set-upstream git@gitlab.com:robert.bardeanu/$(git rev-parse --show-toplevel | xargs basename).git $(git rev-parse --abbrev-ref HEAD)

---
+++ src/Fenster.java
@@ -0,0 +1,20 @@
+import  javax.swing.*;
+public class Fenster {
+    public static void main(String[] args) {
+new  Fenster();
+
+
+    }
+    public Fenster() {
+        int breite=750;
+        int hoehe=250;
+        JFrame fenster=new JFrame("Pinguin Run");
+        fenster.setVisible(true);
+        fenster.setSize(breite,hoehe);
+        fenster.setLocationRelativeTo(null);
+        fenster.setResizable(false);
+        fenster.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ImageIcon icon = new ImageIcon("src\\Media\\Bilder\\Icon.png");
+        fenster.setIconImage(icon.getImage());
+    }
+}
